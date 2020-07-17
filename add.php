f3db2f5db4b63b7654ab97e6958bbda1fe63de0c
nenadlackovic <58701823+nenadlackovic@users.noreply.github.com>
2020-07-17 09:01:34 +0200

    Update add.php
    extended numbers of movies

--- add.php
+++ add.php
@@ -2,20 +2,20 @@
 include('connection.php');
 
 
-$json = file_get_contents('https://api.themoviedb.org/3/movie/popular?api_key=d8bf019d0cca372bd804735f172f67e8');
+for($p = 1; $p <= 5; $p++){
+    $json = file_get_contents('https://api.themoviedb.org/3/movie/popular?api_key=d8bf019d0cca372bd804735f172f67e8&page='.$p);
+    $obj = json_decode($json);
 
-
-$obj = json_decode($json);
-
-
-$count = 0;
-for ($x = 0; $x <= 18; $x++){
-    $name =  $obj->results[$x]->title;
-    $date =  $obj->results[$x]->release_date;
-    $overview = $obj->results[$x]->overview;
-    $img = $obj->results[$x]->poster_path;
-    $mysqli->query("insert into popularmovies (title,release_date,overview,poster_path) values ('$name','$date','$overview','$img')");
+    $count = 0;
+    for ($x = 0; $x <= 18; $x++){
+        $name =  $obj->results[$x]->title;
+        $date =  $obj->results[$x]->release_date;
+        $overview = $obj->results[$x]->overview;
+        $img = $obj->results[$x]->poster_path;
+        $mysqli->query("insert into popularmovies (title,release_date,overview,poster_path) values ('$name','$date','$overview','$img')");
 }
+}
+
 
 ?>
 
