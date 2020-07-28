36c65e9ab5824a554771997625d50b26892c4669
nenadlackovic <58701823+nenadlackovic@users.noreply.github.com>
2020-07-28 14:11:55 +0200

    Final format

--- connection.php
+++ connection.php
@@ -1,12 +1,10 @@
 <?php
 
-  $mysqli2 = mysqli_connect("localhost", "root", "", "iteh1");
+$mysqli2 = mysqli_connect("localhost", "root", "", "iteh1");
 
-  
-  session_start();
-  if(!isset($_SESSION['user'])){
+session_start();
+if (!isset($_SESSION['user'])) {
     $_SESSION['user'] = [];
-  }
+}
 
-
- ?>
+?>
